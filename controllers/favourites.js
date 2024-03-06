83eaeeab8c1a3f5a88546c14dbc2a424371028b3
hayley <hayleyhkim@gmail.com>
2024-03-07 09:24:19 +1100

    favourite page update

--- controllers/favourites.js
+++ controllers/favourites.js
@@ -1,4 +1,5 @@
 const Favourite = require('../models/favourite');
+const Recipe = require('../models/recipe');
 
 module.exports = {
     index,
@@ -8,8 +9,14 @@ module.exports = {
 
   async function index(req, res) {
     try {
+        //use .populate() to replace recipe with actual docs from Recipe collection. - allowing to access full content of Recipe model. (in this case, not only just ID of recipe but the entire Recipe schema)
         const favourites = await Favourite.find({user: req.user._id}).populate('recipe');
-        res.render('favourites/index', {title: 'My Favourites', favourites});
+
+        const fave = await Favourite.find({user: req.user._id});
+
+        const favouritesByCountry = groupRecipesByCountry(favourites.map(favourite => favourite.recipe));
+
+        res.render('favourites/index', { title: 'My Favourites', fave, favouritesByCountry });
     } catch (err) {
         console.log(err);
     }
@@ -40,4 +47,20 @@ module.exports = {
     } catch (err) {
         console.error(err);
     }
+}
+
+function groupRecipesByCountry(recipes) {
+    const recipesByCountry = {};
+
+    recipes.forEach((recipe) => {
+        const country = recipe.country;
+
+        if (!recipesByCountry[country]) {
+            recipesByCountry[country] = [];
+        }
+
+        recipesByCountry[country].push(recipe);
+    });
+
+    return recipesByCountry;
 }
